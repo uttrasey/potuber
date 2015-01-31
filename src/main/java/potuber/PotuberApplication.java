fd33881480dbac6ba0551c914fbfc3f79bbf913f
Matt Uttridge <matt@trazyinc.com>
2015-01-31 12:35:28 +0000

    Spring boot setup of the new backend module

---
+++ src/main/java/potuber/PotuberApplication.java
@@ -0,0 +1,12 @@
+package potuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotuberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotuberApplication.class, args);
+    }
+}
